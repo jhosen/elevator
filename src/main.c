86ac0e9bc2150ced01ae01b1e0a0e11d5556f2f6
Jesper Hosen <jesperho@stud.ntnu.no>
2013-02-23 19:23:15 +0100

    Added mainfile

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "network.h"
+
+int main (int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+	network_init(); 
+    return 0;
+}
